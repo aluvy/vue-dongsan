ceda69284a3302d47aa7945ee7b0d186644c8559
miok <aluvy_@naver.com>
2023-06-13 10:05:17 +0900

    이미지, v-if 모달창, 데이터 상태변경

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,9 +1,19 @@
 <template>
+  <div class="black-bg" v-if="모달창열렸니==1">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="모달창열렸니=0">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(a,i) in 메뉴들" :key="i">{{a}}</a>
   </div>
+
   <div v-for="(a,i) in products" :key="i">
-    <h4>{{ a }}</h4>
+    <img :src="require(`@/assets/room${i}.jpg`)" class="room-img">
+    <h4 @click="모달창열렸니=1">{{ a }}</h4>
     <p>40 만원</p>
     <button @click="신고수[i]++">허위매물신고</button> <span>신고수: {{ 신고수[i] }}</span>
   </div>
@@ -15,15 +25,13 @@ export default {
   name: 'App',
   data(){
     return {
-      // 데이터 보관함
-      // 데이터는 object 자료형으로 저장한다. {자료이름:자료내용}
       메뉴들: ['Home', 'Shop', 'About'],
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
       신고수: [0, 0, 0],
+      모달창열렸니: 0,  // 0:닫힘, 1:열림
     }
   },
   methods: {
-    // 함수 만드는 공간
   },
   components: {
   }
@@ -31,6 +39,9 @@ export default {
 </script>
 
 <style>
+body {margin:0;}
+div{box-sizing:border-box;}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -41,4 +52,10 @@ export default {
 
 .menu{background:darkslateblue; padding:15px; border-radius:5px;}
 .menu a{color:#fff; padding:10px;}
+
+.room-img{width:100%; margin-top:40px;}
+
+/* modal */
+.black-bg{position:fixed; padding:20px; width:100%; height:100%; background:rgba(0,0,0, 0.5);}
+.white-bg{width:100%; background:#fff; border-radius:8px; padding:20px;}
 </style>
