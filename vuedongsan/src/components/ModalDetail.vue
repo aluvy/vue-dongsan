214f63fe1121350cb5b88e9071ec27d05da6fdfb
miok <mosong@the-51.com>
2023-06-16 10:38:43 +0900

    v-model

--- vuedongsan/src/components/ModalDetail.vue
+++ vuedongsan/src/components/ModalDetail.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="black-bg" v-if="모달창열렸니==1">
+  <div class="modal_wrap black-bg" v-if="모달창열렸니==1">
     <div class="white-bg">
       <img :src="원룸들[누른거].image" class="room-img">
       <h4>{{ 원룸들[누른거].title }}</h4>
       <p>{{ 원룸들[누른거].content }}</p>
-      <p>{{ 원룸들[누른거].price }}</p>
+      <div>개월 수 : <input type="text" v-model.number="month"></div>
+      <textarea v-model="month"></textarea>
+      <p>[{{month}}개월 선택] {{ 원룸들[누른거].price * month }}원</p>
       <button @click="closeModal">닫기</button>
     </div>
   </div>
@@ -13,6 +15,11 @@
 <script>
 export default {
   name: 'ModalDetail',
+  data(){
+    return {
+      month: 1,
+    }
+  },
   props: {
     원룸들: Object, // Number, String, Array
     누른거: Number,
@@ -28,6 +35,7 @@ export default {
 
 <style>
 /* modal */
-.black-bg{position:fixed; padding:20px; width:100%; height:100%; background:rgba(0,0,0, 0.5);}
-.white-bg{width:100%; background:#fff; border-radius:8px; padding:20px;}
+.modal_wrap{position:fixed; padding:20px; width:100%; height:100%; background:rgba(0,0,0, 0.5);}
+.modal_wrap .white-bg{width:100%; background:#fff; border-radius:8px; padding:20px;}
+.modal_wrap input{width:40px;}
 </style>
