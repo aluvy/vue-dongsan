a4fcb5c2cffdf90d8ab61e5440172204f19758be
miok <mosong@the-51.com>
2023-06-16 11:42:00 +0900

    vue Transition

--- vuedongsan/src/components/ModalDetail.vue
+++ vuedongsan/src/components/ModalDetail.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="modal_wrap black-bg" v-if="모달창열렸니==1">
-    <div class="white-bg">
-      <img :src="원룸들[누른거].image" class="room-img">
-      <h4>{{ 원룸들[누른거].title }}</h4>
-      <p>{{ 원룸들[누른거].content }}</p>
-      <div>개월 수 : <input type="text" v-model.number="month"></div>
-      <p>[{{month}}개월 선택] {{ 원룸들[누른거].price * month }}원</p>
-      <button @click="closeModal">닫기</button>
+  <Transition name="fade">
+    <div class="modal_wrap black-bg" v-if="모달창열렸니==1">
+      <div class="white-bg">
+        <img :src="원룸들[누른거].image" class="room-img">
+        <h4>{{ 원룸들[누른거].title }}</h4>
+        <p>{{ 원룸들[누른거].content }}</p>
+        <div>개월 수 : <input type="text" v-model="month"></div>
+        <p>[{{month}}개월 선택] {{ 원룸들[누른거].price * month }}원</p>
+        <button @click="closeModal">닫기</button>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <script>
@@ -17,6 +19,18 @@ export default {
   data(){
     return {
       month: 1,
+    }
+  },
+  watch: {
+    month(a, b){  // 감시할 데이터 이름으로 함수를 만든다. (a:변경후 데이터, b:변경 전 데이터)
+      if(a >= 13 ){
+        alert("경고");
+        this.month=b;
+      }
+      if( isNaN(a) ){
+         alert("문자 입력 금지");
+         this.month=b;
+      }
     }
   },
   props: {
@@ -37,4 +51,13 @@ export default {
 .modal_wrap{position:fixed; padding:20px; width:100%; height:100%; background:rgba(0,0,0, 0.5);}
 .modal_wrap .white-bg{width:100%; background:#fff; border-radius:8px; padding:20px;}
 .modal_wrap input{width:40px;}
+
+
+.fade-enter-from{opacity:0;}
+.fade-enter-active{transition:all .3s;}
+.fade-enter-to{opacity:1;}
+
+.fade-leave-from{opacity:1;}
+.fade-leave-active{transition:all .3s;}
+.fade-leave-to{opacity:0;}
 </style>
