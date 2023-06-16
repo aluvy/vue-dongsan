470aff2fbb5f472f6645427440ab7227bc05b647
miok <mosong@the-51.com>
2023-06-16 14:22:16 +0900

    정렬

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -8,6 +8,12 @@
 
   <DiscountBanner />
 
+  <button type="button" @click="sortBack">전체</button>
+  <button type="button" @click="priceSortDesc">가격 내림차순 정렬</button>
+  <button type="button" @click="priceSortAsc">가격 오름차순 정렬</button>
+  <button type="button" @click="ProductSort">가나다순 정렬</button>
+  
+
   <ProductCard @openModal="모달창열렸니=1; 누른거=$event;" v-for="(a,i) in 원룸들" :key="i" :원룸="원룸들[i]" />
 
 </template>
@@ -25,11 +31,35 @@ export default {
       오브젝트: {name: 'kim', age: 20},
       메뉴들: ['Home', 'Shop', 'About'],
       모달창열렸니: 0,  // 0:닫힘, 1:열림
+      원룸들오리지널: [...data],
       원룸들: data,
       누른거: 0,
     }
   },
   methods: {
+    sortBack(){
+      this.원룸들 = [...this.원룸들오리지널];
+    },
+    priceSortDesc(){
+      this.원룸들.sort(function(a,b){
+        return a.price - b.price;
+      });
+    },
+    priceSortAsc(){
+      this.원룸들.sort(function(a,b){
+        return b.price - a.price;
+      });
+    },
+    ProductSort(){
+      this.원룸들.sort(function(a, b){
+        if(a.title.toLowerCase() > b.title.toLowerCase())
+          return 1;
+        else if(a.title.toLowerCase() < b.title.toLowerCase())
+          return -1;
+        else
+          return 0;
+      });
+    }
   },
   components: {
     DiscountBanner : DiscountBanner,
